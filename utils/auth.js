7feeb4a1ec596ce1a935fbae95076e8f41497a8c
CChampness <87551272+CChampness@users.noreply.github.com>
2021-11-27 16:22:09 -0500

    Initial directory layout

---
+++ utils/auth.js
@@ -0,0 +1,13 @@
+const withAuth = (req, res, next) => {
+  // If the user is not logged in, redirect the user to the login page
+  // This is directly from the `/blog/:id` and `/user/:id` routes
+  if (!req.session.loggedIn) {
+    res.redirect('/login');
+  } else {
+    // If the user is logged in, execute the route function that will allow them to view the blog
+    // We call next() if the user is authenticated
+    next();
+  }
+};
+
+module.exports = withAuth;
