5ad8c715af5276d12ad6bda1f74026f334b3671a
CChampness <87551272+CChampness@users.noreply.github.com>
2021-12-03 20:24:25 -0500

    added route to add a post

--- controllers/api/index.js
+++ controllers/api/index.js
@@ -2,6 +2,7 @@ const router = require('express').Router();
 
 const userRoutes = require('./user-routes');
 
+//  /api/users/<endpoint>
 router.use('/users', userRoutes);
 
 module.exports = router;
--- controllers/api/user-routes.js
+++ controllers/api/user-routes.js
@@ -1,5 +1,5 @@
 const router = require('express').Router();
-const { User } = require('../../models');
+const { User, Post, Comment } = require('../../models');
 
 // CREATE new user
 router.post('/', async (req, res) => {
@@ -12,7 +12,7 @@ router.post('/', async (req, res) => {
     });
 
     req.session.save(() => {
-      // req.session.user_id = dbUserData.id;
+      req.session.user_id = dbUserData.id;
       req.session.loggedIn = true;
 
       res.status(200).json(dbUserData);
@@ -79,6 +79,7 @@ router.post('/logout', (req, res) => {
 // Use the custom middleware before allowing the user to access this route
 router.post('/newcomment', async (req, res) => {
   console.log(">>>>>>>>>>>>>> newcomment post route <<<<<<<<<<<<<<<<");
+  console.log(">>>>>>>>>>>>>> SESSION:",req.session);
   try {
     const dbCommentData = await Comment.create({
       commenter: req.body.commenter,
@@ -86,15 +87,35 @@ router.post('/newcomment', async (req, res) => {
       comment_text: req.body.comment_text,
       post_id: req.body.post_id
     });
+    res.status(200).render('post');  //, { post, loggedIn: req.session.loggedIn });
 
-    // const post = dbPostData.get({ plain: true });
-    // console.log(post);
-    // res.render('post', { post, loggedIn: req.session.loggedIn });
+    // res.status(200).json(dbCommentData);
   } catch (err) {
     console.log(err);
     res.status(500).json(err);
   }
 });
 
+// Create a new post
+// Use the custom middleware before allowing the user to access this route
+router.post('/newpost', async (req, res) => {
+  console.log(">>>>>>>>>>>>>> newpost post route <<<<<<<<<<<<<<<<");
+  console.log(">>>>>>>>>>>>>> SESSION:",req.session);
+  try {
+    const dbPostData = await Post.create({
+      title: req.body.title,
+      blogger: req.body.blogger,
+      post_date: req.body.post_date,
+      content: req.body.content,
+      topic_id: req.body.topic_id
+    });
+    res.status(200).render('dash');  //, { post, loggedIn: req.session.loggedIn });
+
+    // res.status(200).json(dbCommentData);
+  } catch (err) {
+    console.log(err);
+    res.status(500).json(err);
+  }
+});
 
 module.exports = router;
